df6691b5668d08b944da1c1e42653cb5a0613af0
CHCheshire <pringl31@gmail.com>
2022-11-28 20:02:21 +0000

    Migrate profile model and create Superuser

--- profiles/models.py
+++ profiles/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 # Create your models here.
 
@@ -16,5 +17,13 @@ class Profile(models.Model):
     class Meta:
         ordering = ['-created_at']
 
-        def __str__(self):
-            return f"{self.owner}'s profile"
+    def __str__(self):
+        return f"{self.owner}'s profile"
+
+
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(owner=instance)
+
+
+post_save.connect(create_profile, sender=User)
