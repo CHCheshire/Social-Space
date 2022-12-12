02398f5268ca69d31fc52143603483dcc80e4360
CHCheshire <pringl31@gmail.com>
2022-12-12 21:01:10 +0000

    Add CommentDetail Model

--- comments/models.py
+++ comments/models.py
@@ -9,12 +9,12 @@ class Comment(models.Model):
     """
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
 
     class Meta:
-        ordering = ['-created_at']
+        ordering = ['-created_on']
 
     def __str__(self):
         return self.content
