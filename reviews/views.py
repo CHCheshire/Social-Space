4b6123b06da3ca1bb59734073fbb4817d2a79cd6
CHCheshire <pringl31@gmail.com>
2022-12-20 15:33:25 +0000

    Add review views

--- reviews/views.py
+++ reviews/views.py
@@ -1,21 +1,67 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from .models import Review
+from .serializers import ReviewSerializer
+from social_space.permissions import IsOwnerOrReadOnly
+from rest_framework.response import Response
+from rest_framework import permissions
+from django.http import Http404
 # Create your views here.
 
 
-class reviewList(APIView):
+class ReviewList(APIView):
 
-    def index(request: HttpRequest) -> HttpResponse:
-        review = Review.objects.all()
-        for post in posts:
-            rating = Rating.objects.filter(post=post, user=request.user).first()
-            post.user_rating = rating.rating if rating else 0
-        return render(request, "index.html", {"posts": posts})
+    serializer_class = ReviewSerializer
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly
+    ]
 
-    def rate(request: HttpRequest, post_id: int, rating: int) -> HttpResponse:
-        post = Post.objects.get(id=post_id)
-        Rating.objects.filter(post=post, user=request.user).delete()
-        post.rating_set.create(user=request.user, rating=rating)
-        return index(request)
+    def review(self, request):
+        serializer = ReviewSerializer(
+            data=request.data, context={'request': request}
+        )
+        if serializer.is_valid():
+            serializer.save(author=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(
+            serializer.errors, status=status.HTTP_400_BAD_REQUEST
+        )
 
+
+class ReviewDetail(APIView):
+    permission_classes = {IsOwnerOrReadOnly}
+    serializer_class = ReviewSerializer
+
+    def get_object(self, pk):
+        try:
+            review = Review.objects.get(pk=pk)
+            self.check_object_permissions(self.request, review)
+            return review
+        except Review.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        review = self.get_object(pk)
+        serializer = ReviewSerializer(
+            review, context={'request': request}
+            )
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        review = self.get_object(pk)
+        serializer = ReviewSerializer(
+            review, data=request.data, context={'request': request}
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(
+            serializer.errors, status=status.HTTP_400_BAD_REQUEST
+        )
+
+    def delete(self, request, pk):
+        review = self.get_object(pk)
+        review.delete()
+        return Response(
+            status=status.HTTP_204_NO_CONTENT
+        )
