df6283fd3f62cf0424cce97c836c4e59810eba90
CHCheshire <pringl31@gmail.com>
2023-03-02 15:53:49 +0000

    Remove filterbackends from comments

--- comments/views.py
+++ comments/views.py
@@ -1,5 +1,4 @@
 from rest_framework import generics, permissions
-from django_filters.rest_framework import DjangoFilterBackend
 from social_space.permissions import IsOwnerOrReadOnly
 from .models import Comment
 from .serializers import CommentSerializer, CommentDetailSerializer
@@ -12,7 +11,6 @@ class CommentList(generics.ListCreateAPIView):
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Comment.objects.all()
-    filter_backends = [DjangoFilterBackend]
     filterset_fields = ['post']
 
     def perform_create(self, serializer):
