abb222f8d34f8a415af97c5c9b66c406b9ec95d6
CHCheshire <pringl31@gmail.com>
2023-03-02 15:43:44 +0000

    Add DjangoFilterBackend to comment views

--- comments/views.py
+++ comments/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics, permissions
+from django_filters.rest_framework import DjangoFilterBackend
 from social_space.permissions import IsOwnerOrReadOnly
 from .models import Comment
 from .serializers import CommentSerializer, CommentDetailSerializer
